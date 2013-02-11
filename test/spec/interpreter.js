a2ddc57b1ac527f5092bbd93faf39f44102ec319
Daan Mortier <tjoekbezoer@gmail.com>
2013-02-11 12:44:31 +0100

    Simplified test calls
    Since pretty much all test use deepEqual, added eq() to shorten the function calls.

--- test/spec/interpreter.js
+++ test/spec/interpreter.js
@@ -31,95 +31,95 @@
 	
 	module('Interpreter');
 	test('executes unary operations correctly', function() {
-		deepEqual(jsq('-1, ~1, !1, -2'), [-1,-2,false,-2]);
-		deepEqual(jsq('1--1'), [2]);
-		deepEqual(jsq('0>-1, 1--1'), [true,2]);
+		eq('-1, ~1, !1, -2', [-1,-2,false,-2]);
+		eq('1--1', [2]);
+		eq('0>-1, 1--1', [true,2]);
 	});
 	
 	test('executes binary operations correctly', function() {
 		// Arithmetic
-		deepEqual(jsq('1+2'), [3]);
-		deepEqual(jsq('2-1'), [1]);
-		deepEqual(jsq('2*3'), [6]);
-		deepEqual(jsq('1.5*2'), [3]);
-		deepEqual(jsq('9/3'), [3]);
-		deepEqual(jsq('6/4'), [1.5]);
+		eq('1+2', [3]);
+		eq('2-1', [1]);
+		eq('2*3', [6]);
+		eq('1.5*2', [3]);
+		eq('9/3', [3]);
+		eq('6/4', [1.5]);
 		// Precedence
-		deepEqual(jsq('1+2*3+2'), [9]);
-		deepEqual(jsq('(1+2)*3+2'), [11]);
-		deepEqual(jsq('1+4/2+1'), [4]);
-		deepEqual(jsq('(2+4)/3+1'), [3]);
-		deepEqual(jsq('((2+4)*2+3)*2'), [30]);
+		eq('1+2*3+2', [9]);
+		eq('(1+2)*3+2', [11]);
+		eq('1+4/2+1', [4]);
+		eq('(2+4)/3+1', [3]);
+		eq('((2+4)*2+3)*2', [30]);
 		
 		// Logical
-		deepEqual(jsq('1&&2, 2&&1, 1 && 2, 1	&&	2'), [2,1,2,2]);
-		deepEqual(jsq('1&&0, 0&&1'), [0,0]);
-		deepEqual(jsq('1||2, 2||1, 1||0'), [1,2,1]);
-		deepEqual(jsq('1==1, 2!=1, 2>1, 2>=1, 1>=1, 1<2, 1<=2, 1<=1'), [true,true,true,true,true,true,true,true]);
-		deepEqual(jsq('2==1, 1!=1, 1>2, 1>=2, 2<1, 2<=1'), [false,false,false,false,false,false]);
+		eq('1&&2, 2&&1, 1 && 2, 1	&&	2', [2,1,2,2]);
+		eq('1&&0, 0&&1', [0,0]);
+		eq('1||2, 2||1, 1||0', [1,2,1]);
+		eq('1==1, 2!=1, 2>1, 2>=1, 1>=1, 1<2, 1<=2, 1<=1', [true,true,true,true,true,true,true,true]);
+		eq('2==1, 1!=1, 1>2, 1>=2, 2<1, 2<=1', [false,false,false,false,false,false]);
 		// Precedence
-		deepEqual(jsq('1||2&&3'), [1]);
-		deepEqual(jsq('(1||2)&&3'), [3]);
-		deepEqual(jsq('0||2&&3'), [3]);
+		eq('1||2&&3', [1]);
+		eq('(1||2)&&3', [3]);
+		eq('0||2&&3', [3]);
 		
 		// Bitwise
-		deepEqual(jsq('1\\2'), [3]);
-		deepEqual(jsq('3&7'), [3]);
-		deepEqual(jsq('1^2, 1^3'), [3,2]);
+		eq('1\\2', [3]);
+		eq('3&7', [3]);
+		eq('1^2, 1^3', [3,2]);
 		// Precedence
-		deepEqual(jsq('2\\5&4, (2\\5)&4'), [6,4]);
-		deepEqual(jsq('2&6\\8, 2&(6\\8)'), [10,2]);
-		deepEqual(jsq('7^5&4, (7^5)&4'), [3,0]);
-		deepEqual(jsq('6&4^8, 6&(4^8)'), [12,4]);
-		deepEqual(jsq('7^5\\4, 7^(5\\4)'), [6,2]);
-		deepEqual(jsq('6\\5^4, (6\\5)^4'), [7,3]);
+		eq('2\\5&4, (2\\5)&4', [6,4]);
+		eq('2&6\\8, 2&(6\\8)', [10,2]);
+		eq('7^5&4, (7^5)&4', [3,0]);
+		eq('6&4^8, 6&(4^8)', [12,4]);
+		eq('7^5\\4, 7^(5\\4)', [6,2]);
+		eq('6\\5^4, (6\\5)^4', [7,3]);
 	});
 
 	test('executes filters correctly', function() {
 		// Simple object
-		deepEqual(jsq(simple, '.'), [simple]);
-		deepEqual(jsq(simple, '.[]'), [1,2,3,4]);
-		deepEqual(jsq(simple, '.["second"]'), [2]);
-		deepEqual(jsq(simple, '.second'), [2]);
+		eq(simple, '.', [simple]);
+		eq(simple, '.[]', [1,2,3,4]);
+		eq(simple, '.["second"]', [2]);
+		eq(simple, '.second', [2]);
 		
 		// Complex object
-		deepEqual(jsq(complex, '.[]'), [1,2,3,complex['fourth'],complex['fifth'],complex["sixth"]]);
-		deepEqual(jsq(complex, '.["fifth"]["baz"], .["fifth"].baz, .fifth["baz"], .fifth.baz'), [3,3,3,3]);
-		deepEqual(jsq(complex, '.fifth[]'), ['bar',3]);
-		deepEqual(jsq(complex, '.fourth'), [complex["fourth"]]);
-		deepEqual(jsq(complex, '.fourth[]'), complex["fourth"]);
+		eq(complex, '.[]', [1,2,3,complex['fourth'],complex['fifth'],complex["sixth"]]);
+		eq(complex, '.["fifth"]["baz"], .["fifth"].baz, .fifth["baz"], .fifth.baz', [3,3,3,3]);
+		eq(complex, '.fifth[]', ['bar',3]);
+		eq(complex, '.fourth', [complex["fourth"]]);
+		eq(complex, '.fourth[]', complex["fourth"]);
 		// Expression as key
-		deepEqual(jsq(complex, '.fourth[.fourth[0]]'), [2]);
-		deepEqual(jsq(complex, '.[.sixth[]]'), [1,2]);
+		eq(complex, '.fourth[.fourth[0]]', [2]);
+		eq(complex, '.[.sixth[]]', [1,2]);
 	});
 
 	test('executes pipes correctly', function() {
-		deepEqual(jsq('2 | .'), [2]);
-		deepEqual(jsq('1,2 | .'), [1,2]);
-		deepEqual(jsq('[1,2] | .'), [[1,2]]);
+		eq('2 | .', [2]);
+		eq('1,2 | .', [1,2]);
+		eq('[1,2] | .', [[1,2]]);
 		deepEqual(jsq(complex, '.fifth | .baz'), jsq(complex, '.fifth.baz'));
 		deepEqual(jsq(complex, '.[]'), jsq(complex, '. | .[]'));
 		deepEqual(jsq(complex, '.fifth[]'), jsq(complex, '.fifth | .[]'));
 	});
 
 	test('executes comma separated values correctly', function() {
-		deepEqual(jsq('1,2,3'), [1,2,3]);
-		deepEqual(jsq('1,2,3 | .'), [1,2,3]);
-		deepEqual(jsq('1,2,3 | .[]'), []);
-		deepEqual(jsq('[1,2,3] | .[]'), [1,2,3]);
-		deepEqual(jsq([1,2,3,4,5,6], '.[1,2,3]'), [2,3,4]);
+		eq('1,2,3', [1,2,3]);
+		eq('1,2,3 | .', [1,2,3]);
+		eq('1,2,3 | .[]', []);
+		eq('[1,2,3] | .[]', [1,2,3]);
+		eq([1,2,3,4,5,6], '.[1,2,3]', [2,3,4]);
 		
-		deepEqual(jsq('1,1\\2,2+1,4'), [1,3,3,4]);
+		eq('1,1\\2,2+1,4', [1,3,3,4]);
 	});
 	
 	test('executes collections correctly', function() {
-		deepEqual(jsq('[1,2]'), [[1,2]]);
-		deepEqual(jsq('[[1,2],[3,4]]'), [[[1,2],[3,4]]]);
-		deepEqual(jsq(simple, '[.[]]'), [[1,2,3,4]]);
+		eq('[1,2]', [[1,2]]);
+		eq('[[1,2],[3,4]]', [[[1,2],[3,4]]]);
+		eq(simple, '[.[]]', [[1,2,3,4]]);
 	});
 	
 	test('executes object creation correctly', function() {
-		deepEqual(jsq('{"foo":1, "neg": -1, "bar":[1,2,3], "name":"baz", "bool":true, "pos":!!2, "last":~1}'), [{
+		eq('{"foo":1, "neg": -1, "bar":[1,2,3], "name":"baz", "bool":true, "pos":!!2, "last":~1}', [{
 			'foo': 1,
 			'neg': -1,
 			'bar': [1,2,3],
@@ -128,32 +128,32 @@
 			'pos': true,
 			'last': -2
 		}], 'With simple values');
-		deepEqual(jsq([1,2], '{first: .}'), [{'first': [1,2]}], 'Using filter as element value');
-		deepEqual(jsq([1,2], '{first: .[]}'), [{'first': 1}, {'first': 2}], 'Using filter that produces multiple results as value');
-		deepEqual(jsq('{"first":1, "second":(1,2)}'), [{
+		eq([1,2], '{first: .}', [{'first': [1,2]}], 'Using filter as element value');
+		eq([1,2], '{first: .[]}', [{'first': 1}, {'first': 2}], 'Using filter that produces multiple results as value');
+		eq('{"first":1, "second":(1,2)}', [{
 			'first': 1,
 			'second': 1
 		}, {
 			'first': 1,
 			'second': 2
 		}], 'Using inline expression that produces multiple results as value');
-		deepEqual(jsq('{"first":(1,2), "second":(3,4)}'), [
+		eq('{"first":(1,2), "second":(3,4)}', [
 			{'first':1, 'second':3},
 			{'first':1, 'second':4},
 			{'first':2, 'second':3},
 			{'first':2, 'second':4}
 		], 'Using multiple inline expressions that produce multiple results: cartesian product');
-		deepEqual(jsq({"third":3, "2":true}, '{"first":1, "second":1,third,2}'), [{
+		eq({"third":3, "2":true}, '{"first":1, "second":1,third,2}', [{
 			'first': 1,
 			'second': 1,
 			'third': 3,
 			'2': true
 		}], 'Shorthand element definitions');
-		deepEqual(jsq({}, '{"first":1, "second":1,third,2}'), [{
+		eq({}, '{"first":1, "second":1,third,2}', [{
 			'first': 1,
 			'second': 1
 		}], 'Non-matching shorthand element definitions are ignored when input is object');
-		deepEqual(jsq([], '{"first":1, "second":1,third,2}'), [{
+		eq([], '{"first":1, "second":1,third,2}', [{
 			'first': 1,
 			'second': 1
 		}], 'Non-matching shorthand element definitions are ignored when input is array');
